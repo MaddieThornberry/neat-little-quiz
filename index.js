ce3c3bb0b40c52511e79681c7b155cf88811b590
maddie <madeleinethornberry@gmail.com>
2022-07-30 13:53:19 -0400

    questions added

---
+++ index.js
@@ -0,0 +1,35 @@
+// 1.
+// 2.
+// 3.
+//4.
+//5.
+
+// Commonly used data types DO NOT include:
+// 1. Strings
+// 2. Booleans
+// 3. Alerts
+// 4. Numbers
+
+// The condition of an if/else statement is enclosed within _____:
+// 1. quotes
+// 2. curly brackets
+// 3. parenthesis
+// 4. square brackets
+
+// Arrays in JavaScript can be used to store _____:
+// 1. numbers and strings
+// 2. other arrays
+// 3. booleans
+// 4. all of the above
+
+// String values must be enclosed within _____ when being assigned to variables.
+// 1. commas
+// 2. curly brackets
+// 3. quotes
+// 4. parenthesis
+
+// A very useful tool used during developement and debugging for printing content to the debugger is 
+// 1. JavaScript
+// 2. Booleans
+// 3. for loops
+// 4. console.log
